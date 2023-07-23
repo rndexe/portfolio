73c0c0218539f0f5e157292da424b00099f1251e
rndexe <himanshu.ernde@gmail.com>
2023-07-23 17:40:13 +0530

    Add instagram embeds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,14 @@
 </svelte:head>
 
 <div class="intro-wrapper">
-    <p>Hello, my name is Himanshu.</p>
+    <p>Hello, my name is <span class="text-teal">Himanshu.</span></p>
     <p>
         I am a creative technologist. I make websites, experiment with electronics and do a little bit of everything in
         between.
     </p>
     <p>
-        Contact me on : <a href="mailto:himanshu.ernde@gmail.com" target="_blank">Email</a>,<a
-            href="https://www.instagram.com/rnd.exe/"
-            target="_blank">Instagram</a
-        >
+        Contact me on : <a class="underline" href="mailto:himanshu.ernde@gmail.com" target="_blank">Email</a> or
+        <a class="underline" href="https://www.instagram.com/rnd.exe/" target="_blank">Instagram</a>
     </p>
 </div>
 
@@ -28,5 +26,30 @@
         font-size: var(--size-h1);
         font-family: var(--font-inter);
         margin-bottom: 4rem;
+    }
+    .text-teal {
+        background-color: #00a98d;
+        background-image: linear-gradient(to right, #00a98d, #91e6d4, #00a98d);
+        background-clip: text;
+        background-size: 200%;
+        -webkit-background-clip: text;
+        color: transparent;
+        animation: background-pan 6s linear infinite;
+    }
+
+    .underline {
+        text-decoration: underline;
+        text-decoration-thickness: 4px;
+        text-underline-offset: 8px;
+    }
+
+    @keyframes background-pan {
+        from {
+            background-position: 0% center;
+        }
+
+        to {
+            background-position: -200% center;
+        }
     }
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,17 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
 <header>
     <nav>
         <ul class="menu">
-            <li class="menu-item">
+            <li aria-current={$page.url.pathname === "/" ? "page" : undefined} class="menu-item">
                 <a href="/">Home</a>
             </li>
-            <li class="menu-item">
+            <li aria-current={$page.url.pathname === "/works" ? "page" : undefined} class="menu-item">
                 <a href="/works">Work</a>
             </li>
-            <li class="menu-item">
+            <li aria-current={$page.url.pathname === "/play" ? "page" : undefined} class="menu-item">
                 <a href="/play">Play</a>
             </li>
         </ul>
@@ -16,7 +20,7 @@
 
 <style>
     nav {
-        height: clamp(4.00rem, calc(3.60rem + 2.00vw), 6.00rem);
+        height: clamp(4rem, calc(3.6rem + 2vw), 6rem);
         display: flex;
         justify-content: center;
         align-items: center;
@@ -25,10 +29,16 @@
         display: flex;
         flex-direction: row;
         justify-content: center;
-        gap: clamp(3.00rem, calc(2.80rem + 1.00vw), 4.00rem);
+        gap: clamp(3rem, calc(2.8rem + 1vw), 4rem);
     }
     li a {
         text-transform: uppercase;
         font-size: 1rem;
+    }
+    .menu-item {
+        padding-inline: 0.5rem;
+    }
+    li[aria-current="page"] {
+        background-color: #91e6d4;
     }
 </style>

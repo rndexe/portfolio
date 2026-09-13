375fbb3f0009741c7475bd3d23db28047ee52329
rndexe <6037756+rndexe@users.noreply.github.com>
2026-09-14 00:51:38 +0530

    Update contact text for clarity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 <div class="intro-wrapper">
     <p>Hello, my name is <span class="text-teal">Himanshu.</span></p>
     <p>
-        Contact me on : <a id="contact" class="underline" href="" target="_blank">Email</a>
+        Contact me through <a id="contact" class="underline" href="" target="_blank">Email</a>
     </p>
 </div>
 
